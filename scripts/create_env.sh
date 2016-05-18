7a5cce02e3eb6f9a81900703a6a7e2daf5318326
Masanori Ishigami <masanori.ishigami@gmail.com>
2016-05-18 20:31:12 +0000

    #28 Modify script

--- scripts/create_env.sh
+++ scripts/create_env.sh
@@ -2,10 +2,16 @@
 
 # For details, please refer to http://dev.toppers.jp/trac_user/ev3pf/wiki/DevEnvLinux
 
-set currentPath = pwd
+projectRoot=`pwd`
 
 # Create development environment
 cd hrp2/cfg
 make
 
-cd $currentPath
+cd ${projectRoot}
+
+# Cancel modifications the following *.a
+git checkout -- ./hrp2/sdk/common/library/lib2/lib2-standalone.a
+git checkout -- ./hrp2/sdk/common/library/libcpp-test/libcpp-test-standalone.a
+
+exit 0
